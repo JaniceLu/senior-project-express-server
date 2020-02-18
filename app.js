9e40168985156c61de361f58798cf183f5931431
Janice Lu <jjlu555@gmail.com>
2020-02-18 15:58:10 -0800

    Update app.js

--- app.js
+++ app.js
@@ -17,4 +17,4 @@ app.get('/', function (req, res) {
     });
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Listening on port ${port}!`));
