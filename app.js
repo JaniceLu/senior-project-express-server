dd365ecf79ec2ea121ebba19a5d6c485a5225a02
Janice Lu <jjlu555@gmail.com>
2020-02-18 13:25:44 -0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
+const port = (process.env.PORT || 3000);
+const connection = mysql.createConnection(process.env.DATABASE_URL);
+const SELECT_ALL_PRODUCTS_QUERY = 'SELECT * FROM assignments';
+connection.connect(function(err) { 
+    console.log(process.env.DATABASE_URL);
+    if(err) throw err;
+    console.log("Connected!");
+});
+
+app.get('/', function (req, res) {
+    connection.query(SELECT_ALL_PRODUCTS_QUERY, function (err, rows, fields) {
+        if (err) console.log(err);
+        else { res.send(rows) }
+    });
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
