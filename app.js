d1838669ef4d12809e1617fd30843ae52e5bce30
Janice Lu <jjlu555@gmail.com>
2020-03-11 12:29:57 -0700

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const app = express();
 const cors = require('cors');
 const port = (process.env.PORT || 3000);
 const connection = mysql.createConnection(process.env.DATABASE_URL);
-const SELECT_ALL_PRODUCTS_QUERY = 'SELECT * FROM assignments';
+const SELECT_ALL_PRODUCTS_QUERY = 'SELECT * FROM users';
+const ADD_NEW_USER_QUERY = 'INSERT INTO users (firebase_id, name, school_id, is_teacher, email) VALUES ?'
 app.use(cors());
 connection.connect(function(err) { 
     console.log(process.env.DATABASE_URL);
@@ -13,10 +14,28 @@ connection.connect(function(err) {
 });
 
 app.get('/', function (req, res) {
-    connection.query(SELECT_ALL_PRODUCTS_QUERY, function (err, rows, fields) {
+    connection.query(SELECT_ALL_PRODUCTS_QUERY, function (err, rows) {
         if (err) console.log(err);
         else { res.send(rows) }
     });
-});//hello
+});
+
+app.post('/signup', function(req, res) {
+    var new_user = {
+        firebase_id: req.body.firebase_id,
+        name: req.body.name,
+        school_id: req.body.school_id,
+        is_teacher: req.body.is_teacher,
+        email: req.body.email
+    }
+    connection.query(ADD_NEW_USER_QUERY, new_user, function (err, results, fields) {
+        if (err) res.send("Added user"); 
+        else {
+            console.log("Added user");
+            res.send("Added user");
+            connection.end();
+        }
+    });
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
