41593ea3181a9f649fd7d027262b03dddf2edec5
Janice Lu <jjlu555@gmail.com>
2020-03-11 15:06:00 -0700

    Update app.js

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.post('/signup', function(req, res) {
         email: req.body.email
     }
     connection.query(ADD_NEW_USER_QUERY, new_user, function (err, results) {
-        if (err) res.send(err + new_user.firebase_id + new_user.email);
+        if (err) res.send(err + " " + new_user.firebase_id + " " + new_user.email);
         else res.send(results);
     });
 });
