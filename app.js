185d51c810c69be7d118a8d09e82124ef93e4446
Janice Lu <jjlu555@gmail.com>
2020-03-12 16:13:38 -0700

    added signin

--- app.js
+++ app.js
@@ -6,26 +6,35 @@ const bodyParser = require('body-parser');
 const port = (process.env.PORT || 3000);
 const connection = mysql.createConnection(process.env.DATABASE_URL || process.env.DATABASE_URI);
 const SELECT_ALL_PRODUCTS_QUERY = 'SELECT * FROM users';
-const ADD_NEW_USER_QUERY = 'INSERT INTO users SET ?'
+const ADD_NEW_USER_QUERY = 'INSERT INTO users SET ?';
+const GET_USER_INFO_QUERY = 'SELECT firebase_id, is_teacher from users WHERE firebase_id = ?';
+
 app.use(bodyParser.raw());
 app.use(bodyParser.json());
 app.use(cors());
+
 connection.connect(function(err) { 
     console.log(process.env.DATABASE_URL);
-    if(err) throw err;
+    if(err) {
+        console.log(err);
+        throw err;
+    }
     console.log("Connected!");
 });
 
 app.get('/', function (req, res) {
     connection.query(SELECT_ALL_PRODUCTS_QUERY, function (err, rows) {
         if (err) console.log(err);
-        else { res.send(rows) }
+        else { 
+            console.log(rows); 
+            res.send(rows);
+        }
     });
 });
 
 app.post('/signup', function(req, res) {
     console.log(req.body);
-    var new_user ={
+    var new_user = {
         firebase_id: req.body.firebase_id,
         school_id: req.body.school_id,
         name: req.body.name,
@@ -45,4 +54,25 @@ app.post('/signup', function(req, res) {
     });
 });
 
+app.post('/signin', function(req, res) {
+    console.log(req.body);
+    connection.query(GET_USER_INFO_QUERY, req.body.firebase_id, function (err, results) {
+        if (err) {
+            console.log("didn't work");
+            console.log(err);
+            res.send(err);
+        } else {
+            if(results && results.length) {
+                console.log("user has been found");
+                console.log(results);
+                res.send(results);
+            } else {
+                console.log("no user with firebase id: " + req.body.firebase_id);
+                res.send(results);
+            }
+        }
+    })
+});
+
 app.listen(port, () => console.log(`Listening on port ${port}!`));
+
