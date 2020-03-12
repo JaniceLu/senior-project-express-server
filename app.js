6f7e237673519f55c87d514d48669ca43e7ace6f
Janice Lu <jjlu555@gmail.com>
2020-03-11 18:02:43 -0700

    Update app.js

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.get('/', function (req, res) {
 });
 
 app.post('/signup', function(req, res) {
-    console.log(req.body);
+    console.log(req);
     var new_user = {
         firebase_id: req.body.firebase_id,
         school_id: req.body.school_id,
