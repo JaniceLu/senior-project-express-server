f64ad8894d4efabc3319fe96003696b0673e38dd
Mark <casapao.markkenneth@gmail.com>
2020-03-23 16:57:45 -0700

    Changed export syntax

--- Users/users.js
+++ Users/users.js
@@ -1,91 +1,85 @@
 const ADD_NEW_USER_QUERY = "INSERT INTO users SET ?";
 const addUser = (req, res) => {
-    console.log("Sign Up body given: ");
-    console.log(req.body);
-    var new_user = {
-        firebase_id: req.body.firebase_id,
-        school_id: req.body.school_id,
-        name: req.body.name,
-        is_teacher: req.body.is_teacher,
-        email: req.body.email
-    };
-    connection.query(ADD_NEW_USER_QUERY, new_user, function (err, results) {
-        if (err) {
-            console.log("Error adding user");
-            console.log(err);
-            res.send({ failed: true });
-        } else {
-            console.log("Inserted User");
-            res.send({ failed: false });
-        }
-    });
-}
+  console.log("Sign Up body given: ");
+  console.log(req.body);
+  var new_user = {
+    firebase_id: req.body.firebase_id,
+    school_id: req.body.school_id,
+    name: req.body.name,
+    is_teacher: req.body.is_teacher,
+    email: req.body.email
+  };
+  connection.query(ADD_NEW_USER_QUERY, new_user, function(err, results) {
+    if (err) {
+      console.log("Error adding user");
+      console.log(err);
+      res.send({ failed: true });
+    } else {
+      console.log("Inserted User");
+      res.send({ failed: false });
+    }
+  });
+};
 
 const GET_USER_INFO_QUERY = "SELECT * from users WHERE firebase_id = ?";
 const getUserInfo = (req, res) => {
-    console.log("Sign In body given: ");
-    console.log(req.body);
-    connection.query(GET_USER_INFO_QUERY, req.body.firebase_id, function (
-        err,
-        results
-    ) {
-        if (err) {
-            console.log("Error getting user");
-            console.log(err);
-            res.send({ failed: true });
-        } else {
-            if (results && results.length) {
-                console.log("User has been found");
-                console.log(results);
+  console.log("Sign In body given: ");
+  console.log(req.body);
+  connection.query(GET_USER_INFO_QUERY, req.body.firebase_id, function(
+    err,
+    results
+  ) {
+    if (err) {
+      console.log("Error getting user");
+      console.log(err);
+      res.send({ failed: true });
+    } else {
+      if (results && results.length) {
+        console.log("User has been found");
+        console.log(results);
 
-                res.send(results);
-            } else {
-                console.log("No user with firebase id: " + req.body.firebase_id);
-                console.log(results);
-                res.send({ failed: true });
-            }
-        }
-    });
-}
+        res.send(results);
+      } else {
+        console.log("No user with firebase id: " + req.body.firebase_id);
+        console.log(results);
+        res.send({ failed: true });
+      }
+    }
+  });
+};
 
 const UPDATE_USER_INFO_QUERY = "UPDATE users SET ? WHERE firebase_id = ?";
 const updateUserInfo = (req, res) => {
-    console.log("Update Profile body given: ");
-    console.log(req.body);
-    var updatedInfo = {
-        school_id: req.body.school_id,
-        name: req.body.name,
-        email: req.body.email
-    };
-    connection.query(
-        UPDATE_USER_INFO_QUERY,
-        [updatedInfo, req.body.firebase_id],
-        function(err, results) {
-        if (err) {
-            console.log("Error updating profile");
-            console.log(err);
-            res.send({ failed: true });
+  console.log("Update Profile body given: ");
+  console.log(req.body);
+  var updatedInfo = {
+    school_id: req.body.school_id,
+    name: req.body.name,
+    email: req.body.email
+  };
+  connection.query(
+    UPDATE_USER_INFO_QUERY,
+    [updatedInfo, req.body.firebase_id],
+    function(err, results) {
+      if (err) {
+        console.log("Error updating profile");
+        console.log(err);
+        res.send({ failed: true });
+      } else {
+        if (results) {
+          console.log("User info has been changed");
+          console.log(results);
+          res.send({ failed: false });
         } else {
-            if (results) {
-            console.log("User info has been changed");
-            console.log(results);
-            res.send({ failed: false });
-            } else {
-            console.log("User info has not been changed"); //This means the query went through but changed nothing
-            console.log(results);
-            res.send({ failed: true });
-            }
+          console.log("User info has not been changed"); //This means the query went through but changed nothing
+          console.log(results);
+          res.send({ failed: true });
         }
-    });
-}
+      }
+    }
+  );
+};
 
-
-
-
-const _addUser = addUser;
-const _getUserInfo = getUserInfo;
-const _updateUserInfo = updateUserInfo;
-
-export { _addUser as addUser };
-export { _getUserInfo as getUserInfo };
-export { _updateUserInfo as updateUserInfo };
+exports.addUser = addUser;
+exports.getUserInfo = getUserInfo;
+exports.updateUserInfo = updateUserInfo;
--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const mysql = require("mysql");
 const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const moment = require("moment");
-const classes = require('./Classes/classes.js')
-const user = require('./Users/users.js');
+const assignments = require("./Assignments/assignments.js");
+const classes = require("./Classes/classes.js");
+const user = require("./Users/users.js");
 
 const port = process.env.PORT || 3000;
 const dburl = process.env.DATABASE_URL || process.env.DATABASE_URI;
@@ -64,85 +64,6 @@ app.post("/updateclass", classes.updateClass());
 /*
  * Use Case 2.9.1
  */
-const CREATE_ASSIGNMENT_QUERY = `INSERT INTO assignments SET ?`;
-const CREATE_QUESTIONS_QUERY = `INSERT INTO questions (assignment_id,question,answer) VALUES ?`;
-app.post("/createassignment", (req, res) => {
-  /*
-   * Expected Body format:
-   * body: {
-   *    class_id: int,
-   *    name: string,
-   *    due_date: 'YYYY-MM-DD',
-   *    number_of_questions: int,
-   *    questions: [{question: '5 + 5', answer: 10},{question: '10 + 11', answer: 21}]
-   * }
-   */
-  const requestBody = req.body;
-  console.log("Create Assignment body given: ");
-  console.log(requestBody);
-  connection.beginTransaction(function(err) {
-    if (err) {
-      console.log("Error creating assignment transaction");
-      console.log(err);
-      res.send({ failed: true });
-    }
-
-    //Work on inserting the assignments first
-    const assignmentData = {
-      class_id: requestBody.class_id,
-      name: requestBody.name,
-      due_date: requestBody.due_date,
-      pub_date: moment().format("YYYY-MM-DD"),
-      number_of_questions: requestBody.number_of_questions
-    };
-
-    connection.query(CREATE_ASSIGNMENT_QUERY, assignmentData, function(
-      error,
-      results,
-      fields
-    ) {
-      if (error) {
-        return connection.rollback(function() {
-          console.log("Error inserting assignment");
-          console.log(err);
-          res.send({ failed: true });
-        });
-      }
-
-      //MySQL inserting multiple records requires that all data is inside a 2D array
-      const assignmentId = results.insertId;
-      const questionData = requestBody.questions.map(obj => [
-        assignmentId,
-        ...Object.values(obj)
-      ]);
-
-      //If assignment insertion was successful, work on inserting the questions next
-      connection.query(CREATE_QUESTIONS_QUERY, [questionData], function(
-        error,
-        results,
-        fields
-      ) {
-        if (error) {
-          return connection.rollback(function() {
-            console.log("Error inserting questions");
-            console.log(err);
-            res.send({ failed: true });
-          });
-        }
-        connection.commit(function(err) {
-          if (err) {
-            return connection.rollback(function() {
-              console.log("Error commiting create assignment transaction");
-              console.log(err);
-              res.send({ failed: true });
-            });
-          }
-          console.log("Assignment has been added!");
-          res.send({ failed: false });
-        });
-      });
-    });
-  });
-});
+app.post("/createassignment", assignments.createAssignment());
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
